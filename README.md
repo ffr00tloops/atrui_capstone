d1da8f75fafa35295ae972a6c7e6f38029930884
ffr00tloops <banuelosdaniel.db@gmail.com>
2022-10-06 20:49:50 +0800

    first-commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# atrui_capstone
---
+++ client-gatsby/src/pages/index.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+
+const IndexPage = () => {
+  return (
+    <h1 className="text-5xl font-bold">
+      Hello World
+    </h1>
+  )
+}
+
+export default IndexPage
+
+export const Head = () => <title>Home Page</title>
