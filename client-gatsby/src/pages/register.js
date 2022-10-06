8d333198949ced92f637a922c480a94b8257e3bd
ffr00tloops <banuelosdaniel.db@gmail.com>
2022-10-06 22:26:08 +0800

    added register form

--- client-gatsby/src/pages/register.js
+++ client-gatsby/src/pages/register.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 
 
-const LoginPage = () => {
+const RegisterPage = () => {
   return (
     <div className="bg-green-500 lg:h-48 lg:p-48">
         <div className="bg-slate-100 lg:w-6/12  lg:m-auto drop-shadow-2xl lg:rounded-xl border-green-300 mt-32 border-4">
@@ -13,9 +13,11 @@ const LoginPage = () => {
                     <div className="grid grid-cols-1">
                         <label className="m-auto mt-3 mb-3">Username</label>
                         <input className="p-1 m-auto" placeholder="Type your username" />
-                        <label className="m-auto mt-3 mb-3">Password</label>
-                        <input className="p-1 m-auto" placeholder="Type your password" />
-                        <a className="m-auto mt-3 mb-3 text-red-600 text-sm" href="/register">Create an Account?</a>
+                        <label className="m-auto mt-3 mb-3">Confirm Password</label>
+                        <input className="p-1 m-auto" placeholder="Type new password password" />
+                        <label className="m-auto mt-3 mb-3">Retype Password</label>
+                        <input className="p-1 m-auto" placeholder="Confirm password" />
+                        <a className="m-auto mt-3 mb-3 text-red-600 text-sm" href="/register">Already have an Account?</a>
                     </div>
                     <div className="grid grid-cols-2 m-auto">
                         <a href="/login" className="bg-green-300 p-2 rounded-xl mt-6 mb-3 font-bold">Login</a>
@@ -29,6 +31,6 @@ const LoginPage = () => {
   )
 }
 
-export default LoginPage
+export default RegisterPage
 
-export const Head = () => <title>Login</title>
+export const Head = () => <title>Register</title>
