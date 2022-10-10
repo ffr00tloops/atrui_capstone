dbcf274346650b840037141d219c4865f91e4a41
ffr00tloops <banuelosdaniel.db@gmail.com>
2022-10-10 21:16:36 +0800

    added sub menu and menus for homepage when logged

--- client-gatsby/src/pages/home.js
+++ client-gatsby/src/pages/home.js
@@ -1,6 +1,185 @@
 import * as React from "react"
+import {useState} from "react"
 import Footer from '../components/Footer'
 import NavLogged from '../components/NavLogged'
+
+function Fundraisers() {
+  return (
+    <div className="w-10/12 m-auto mt-8 mb-8">
+    <h1 className="text-center">Fundraisers</h1>
+    <div className=" lg:grid lg:grid-cols-3 lg:grid-rows-1 w-10/12 m-auto">
+      <div className="border-2 rounded-sm drop-shadow-lg border-gray-200 m-3">
+        <div className="">
+          <img src="https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder.png"/>
+          <h1 className="text-xl font-bold">Hello World</h1>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+        </div>
+      </div>
+      <div className="border-2 rounded-sm drop-shadow-lg border-gray-200 m-3">
+        <div className="">
+          <img src="https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder.png"/>
+          <h1 className="text-xl font-bold">Hello World</h1>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        </div>
+      </div>
+      <div className="border-2 rounded-sm drop-shadow-lg border-gray-200 m-3">
+        <div className="">
+          <img src="https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder.png"/>
+          <h1 className="text-xl font-bold">Hello World</h1>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+function Dashboard() {
+  return (
+    <div className="w-10/12 m-auto mt-8 mb-8">
+    <h1 className="text-center">Dashboard</h1>
+    <div className=" lg:grid lg:grid-cols-3 lg:grid-rows-1 w-10/12 m-auto">
+      <div className="border-2 rounded-sm drop-shadow-lg border-gray-200 m-3">
+        <div className="">
+          <img src="https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder.png"/>
+          <h1 className="text-xl font-bold">Hello World</h1>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+        </div>
+      </div>
+      <div className="border-2 rounded-sm drop-shadow-lg border-gray-200 m-3">
+        <div className="">
+          <img src="https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder.png"/>
+          <h1 className="text-xl font-bold">Hello World</h1>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        </div>
+      </div>
+      <div className="border-2 rounded-sm drop-shadow-lg border-gray-200 m-3">
+        <div className="">
+          <img src="https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder.png"/>
+          <h1 className="text-xl font-bold">Hello World</h1>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+function Organizers() {
+  return (
+    <div className="w-10/12 m-auto mt-8 mb-8">
+    <h1 className="text-center">Organizers</h1>
+    <div className=" lg:grid lg:grid-cols-3 lg:grid-rows-1 w-10/12 m-auto">
+      <div className="border-2 rounded-sm drop-shadow-lg border-gray-200 m-3">
+        <div className="">
+          <img src="https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder.png"/>
+          <h1 className="text-xl font-bold">Hello World</h1>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+        </div>
+      </div>
+      <div className="border-2 rounded-sm drop-shadow-lg border-gray-200 m-3">
+        <div className="">
+          <img src="https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder.png"/>
+          <h1 className="text-xl font-bold">Hello World</h1>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        </div>
+      </div>
+      <div className="border-2 rounded-sm drop-shadow-lg border-gray-200 m-3">
+        <div className="">
+          <img src="https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder.png"/>
+          <h1 className="text-xl font-bold">Hello World</h1>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+function Feed() {
+  return (
+    <div className="w-10/12 m-auto mt-8 mb-8">
+    <h1 className="text-center">Feed</h1>
+    <div className=" lg:grid lg:grid-cols-3 lg:grid-rows-1 w-10/12 m-auto">
+      <div className="border-2 rounded-sm drop-shadow-lg border-gray-200 m-3">
+        <div className="">
+          <img src="https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder.png"/>
+          <h1 className="text-xl font-bold">Hello World</h1>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+        </div>
+      </div>
+      <div className="border-2 rounded-sm drop-shadow-lg border-gray-200 m-3">
+        <div className="">
+          <img src="https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder.png"/>
+          <h1 className="text-xl font-bold">Hello World</h1>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        </div>
+      </div>
+      <div className="border-2 rounded-sm drop-shadow-lg border-gray-200 m-3">
+        <div className="">
+          <img src="https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder.png"/>
+          <h1 className="text-xl font-bold">Hello World</h1>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+function ContactUs() {
+  return (
+    <div className="w-10/12 m-auto mt-8 mb-8">
+    <h1 className="text-center">Contact Us</h1>
+    <div className=" lg:grid lg:grid-cols-3 lg:grid-rows-1 w-10/12 m-auto">
+        <h1>Lorem</h1>
+    </div>
+  </div>
+  )
+}
+
+// handles logic for when changing menus in the home-loggedin page
+function Nav() {
+  const [location, setLocation] = useState('')
+
+  // this function will trigger which menu is displayed 
+  function renderMenu() {
+    if(location == 'Dashboard') {
+      return <Dashboard />
+    }
+    else if (location == "Fundraisers") {
+      return <Fundraisers />
+    }
+    else if (location == "Organizers") {
+      return <Organizers />
+    }
+    else if (location == "Feed") {
+      return <Feed />
+    }
+    else if (location == "ContactUs") {
+      return <ContactUs/>
+    }
+    else {
+      return <h1>Error</h1>
+    }
+
+  }
+
+
+  return (
+    <div>
+      <div className="w-6/12 m-auto mb-12 grid lg:grid-cols-5">
+        <button onClick={() => setLocation('Dashboard')} className="bg-red-500 border-2 p-3">Dashboard</button>
+        <button onClick={() => setLocation('Fundraisers')} className="bg-red-500 border-2 p-3">Fundraisers</button>
+        <button onClick={() => setLocation('Organizers')} className="bg-red-500 border-2 p-3">Organizers</button>
+        <button onClick={() => setLocation('Feed')} className="bg-red-500 border-2 p-3">Feed</button>
+        <button onClick={() => setLocation('ContactUs')} className="bg-red-500 border-2 p-3">Contact Us</button>
+      </div>
+
+      {renderMenu()}
+    </div>
+  )
+}
 
 
 
@@ -8,6 +187,7 @@ const HomepageLogged = () => {
   return (
     <div>
       <NavLogged />
+      <Nav />
       <Footer />
     </div>
   )
