71432a5a76031fa6c9f73f04bd93f091ee3b9eda
ffr00tloops <banuelosdaniel.db@gmail.com>
2022-10-15 18:26:55 +0800

    connect to db

--- backend/db.sql
+++ backend/db.sql
@@ -1,5 +1,10 @@
-CREATE TABLE users (
+CREATE TABLE fundraisers (
     id SERIAL PRIMARY KEY,
-    username VARCHAR,
-    password VARCHAR
+    title VARCHAR,
+    description VARCHAR,
+    location VARCHAR,
+    datemade DATE,
+    progress INTEGER check (progress between 0 and 100)
 );
+
+INSERT INTO fundraisers(title,description,location) VALUES ('Sample Title', 'Sample Description', "Manila");
--- backend/server.js
+++ backend/server.js
@@ -4,22 +4,11 @@ const app = express()
 const port = 3000
 const cors = require("cors")
 const bodyParser = require('body-parser')
+const pool = require('./db')
 
 
 app.use(express.json())
 
-
-const { auth } = require('express-openid-connect');
-
-
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: 'a long, randomly-generated string stored in env',
-  baseURL: 'http://localhost:3000',
-  clientID: 'FiRzhpf3fjIZpUa11bynijmWSkgRMLkU',
-  issuerBaseURL: 'https://dev--fvxeza3.us.auth0.com'
-};
 
 app.use(auth(config));
 
@@ -28,6 +17,20 @@ app.use(auth(config));
 const userRouter = require('./routes/user')
 
 app.use('/users', userRouter)
+
+app.get("/", async (req, res) => {
+
+    try {
+      const allFundraisers = await pool.query("SELECT * FROM fundraisers")
+        
+      res.json(allFundraisers.rows)
+    }
+    catch(err)
+    { 
+        console.log (err.message)
+    }
+})
+
 
 
 
